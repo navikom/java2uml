e3404aff89de605733ab61c0d478a08a23ae6510
andrej <navikom11@mail.ru>
2014-12-01 13:35:10 +0300

    Verification environment

--- src/com/github/java2uml/Developers.java
+++ src/com/github/java2uml/Developers.java
@@ -4,5 +4,5 @@ package com.github.java2uml;
  * Created by nadcukandrej on 01.12.14.
  */
 public class Developers {
-    public final String LAST_NAME = "Nadchuk";
+    public final String NADCHUK = "Andrei Nadchuk";
 }
--- src/com/github/java2uml/Main.java
+++ src/com/github/java2uml/Main.java
@@ -2,6 +2,8 @@ package com.github.java2uml;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Java2UML starting point...");
+        System.out.println(new Developers().NADCHUK + " is ready for developers!");
     }
 }
